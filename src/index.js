7d8526bfce28574c1165080b0066337ee7522fb4
Orlando <orlando@dataops.co.uk>
2023-04-13 23:22:53 +0100

    JSONada Implementation setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
-//Here we're importing items we'll need. You can add other imports here.
+import jsonata from 'jsonata';
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
+
+
+window.callJSONata = function (json) {
+  const obj = JSON.parse(json);
+  const data = obj.data;
+  const query = obj.query;
+
+  evaluateJSONata(data, query)
+    .then(result => passBackResult(result))
+    .catch(error => console.error(error));
 };
+
+
+
+
+function evaluateJSONata(data, query) {
+  return new Promise((resolve, reject) => {
+    try {
+      const result = jsonata(query).evaluate(data);
+      resolve(result);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+
+function passBackResult(result) {
+  FileMaker.PerformScript ( "set Result" , JSON.stringify(result)) ;
+}
